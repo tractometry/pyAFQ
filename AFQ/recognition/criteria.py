refine numerical corrections in guassion weighting; fix other bundles code to consider affine

AFQ/recognition/criteria.py
@@ -304,7 +304,8 @@ def clean_by_other_bundle(b_sls, bundle_def,
         cleaned_idx_core = abo.clean_relative_to_other_core(
             bundle_def[other_bundle_name]['core'].lower(),
             preproc_imap["fgarray"][b_sls.selected_fiber_idxs],
-            np.array(abu.resample_tg(other_bundle_sls, 20)))
+            np.array(abu.resample_tg(other_bundle_sls, 20)),
+            img.affine)
         cleaned_idx = np.logical_and(cleaned_idx, cleaned_idx_core)
 
     b_sls.select(cleaned_idx, other_bundle_name)

AFQ/recognition/other_bundles.py
@@ -24,7 +24,7 @@ def clean_by_other_density_map(this_bundle_sls,
     return cleaned_idx
 
 
-def clean_relative_to_other_core(core, this_fgarray, other_fgarray):
+def clean_relative_to_other_core(core, this_fgarray, other_fgarray, affine):
     """
     Remove any fibers that are on the wrong side of the core
     """
@@ -46,6 +46,10 @@ def clean_relative_to_other_core(core, this_fgarray, other_fgarray):
     elif core == 'left':
         core_axis = 0
         core_direc = 1
+
+    for ii in range(3):
+        if affine[ii, ii] < 0:
+            core_direc = -core_direc
 
     core_bundle = np.median(other_fgarray, axis=0)
     cleaned_idx_core = np.zeros(this_fgarray.shape[0], dtype=np.bool8)

AFQ/recognition/tests/test_other_bundles.py
@@ -31,7 +31,8 @@ def test_clean_relative_to_other_core():
         cleaned_idx_core = abo.clean_relative_to_other_core(
             core,
             this_bundle_sls_sample,
-            other_bundle_sls_sample
+            other_bundle_sls_sample,
+            np.eye(4)
         )
 
         assert isinstance(cleaned_idx_core, np.ndarray)