[ENH] Use Distance Transform for Include/Exclude Distance calculation; fix ROI dists

AFQ/api/bundle_dict.py
@@ -8,6 +8,7 @@ from AFQ.definitions.utils import find_file
 
 import numpy as np
 import nibabel as nib
+from scipy.ndimage import distance_transform_edt
 
 from dipy.io.streamline import load_tractogram
 
@@ -938,62 +939,16 @@ class BundleDict(MutableMapping):
             resample_subject_to=self.resample_subject_to,
             keep_in_memory=self.keep_in_memory)
 
-    def apply_to_rois(self, b_name, func, *args,
-                      dry_run=False, apply_to_recobundles=False,
-                      **kwargs):
+    def apply_to_rois(self, b_name, *args, **kwargs):
         """
-        Applies some transformation to all ROIs (include, exclude, end, start)
-        and the prob_map in a given bundle.
+        See: AFQ.api.bundle_dict.apply_to_roi_dict
 
         Parameters
         ----------
         b_name : name
             bundle name of bundle whose ROIs will be transformed.
-        func : function
-            function whose first argument must be a Nifti1Image and which
-            returns a Nifti1Image
-        dry_run : bool
-            Whether to actually apply changes returned by `func` to the ROIs.
-            If has_return is False, dry_run is not used.
-        apply_to_recobundles : bool, optional
-            Whether to apply the transformation to recobundles
-            TRKs as well.
-            Default: False
-
-        *args :
-            Additional arguments for func
-        **kwargs
-            Optional arguments for func
-
-        Returns
-        -------
-        A dictionary where keys are
-        the roi type and values are the transformed ROIs.
         """
-        return_vals = {}
-        for roi_type in [
-                "include", "exclude",
-                "start", "end", "prob_map"]:
-            if roi_type in self._dict[b_name]:
-                if roi_type in ["start", "end", "prob_map"]:
-                    return_vals[roi_type] = func(
-                        self._dict[b_name][roi_type], *args, **kwargs)
-                else:
-                    changed_rois = []
-                    for _roi in self._dict[b_name][roi_type]:
-                        changed_rois.append(func(
-                            _roi, *args, **kwargs))
-                    return_vals[roi_type] = changed_rois
-        if apply_to_recobundles and "recobundles" in self._dict[b_name]:
-            return_vals["recobundles"] = {}
-            for sl_type in ["sl", "centroid"]:
-                return_vals["recobundles"][sl_type] = func(
-                    self._dict[b_name]["recobundles"][sl_type],
-                    *args, **kwargs)
-        if not dry_run:
-            for roi_type, roi in return_vals.items():
-                self._dict[b_name][roi_type] = roi
-        return return_vals
+        return apply_to_roi_dict(self._dict[b_name], *args, **kwargs)
 
     def _cond_load_bundle(self, b_name, dry_run=False):
         """
@@ -1020,6 +975,12 @@ class BundleDict(MutableMapping):
     def is_bundle_in_template(self, bundle_name):
         return "space" not in self._dict[bundle_name]\
             or self._dict[bundle_name]["space"] == "template"
+
+    def _roi_distance_helper(self, roi_or_sl):
+        return nib.Nifti1Image(
+            distance_transform_edt(
+                np.where(roi_or_sl.get_fdata() == 0, 1, 0)),
+            roi_or_sl.affine)
 
     def _roi_transform_helper(self, roi_or_sl, mapping,
                               new_affine, bundle_name):
@@ -1138,3 +1099,67 @@ class BundleDict(MutableMapping):
             self.resample_to,
             self.resample_subject_to,
             self.keep_in_memory)
+
+
+def apply_to_roi_dict(dict_, func, *args,
+                      dry_run=False, apply_to_recobundles=False,
+                      apply_to_prob_map=True,
+                      **kwargs):
+    """
+    Applies some transformation to all ROIs (include, exclude, end, start)
+    and the prob_map in a given bundle.
+
+    Parameters
+    ----------
+    dict_: dict
+        dict describing the bundle using pyAFQ's format. An entry
+        in a BundleDict.
+    func : function
+        function whose first argument must be a Nifti1Image and which
+        returns a Nifti1Image
+    dry_run : bool
+        Whether to actually apply changes returned by `func` to the ROIs.
+        If has_return is False, dry_run is not used.
+    apply_to_recobundles : bool, optional
+        Whether to apply the transformation to recobundles
+        TRKs as well.
+        Default: False
+    apply_to_prob_map : bool, optional
+        Whether to apply the transformation to the prob_map.
+        Default: True
+
+    *args :
+        Additional arguments for func
+    **kwargs
+        Optional arguments for func
+
+    Returns
+    -------
+    A dictionary where keys are
+    the roi type and values are the transformed ROIs.
+    """
+    return_vals = {}
+    roi_types = ["include", "exclude", "start", "end"]
+    if apply_to_prob_map:
+        roi_types.append("prob_map")
+    for roi_type in roi_types:
+        if roi_type in dict_:
+            if roi_type in ["start", "end", "prob_map"]:
+                return_vals[roi_type] = func(
+                    dict_[roi_type], *args, **kwargs)
+            else:
+                changed_rois = []
+                for _roi in dict_[roi_type]:
+                    changed_rois.append(func(
+                        _roi, *args, **kwargs))
+                return_vals[roi_type] = changed_rois
+    if apply_to_recobundles and "recobundles" in dict_:
+        return_vals["recobundles"] = {}
+        for sl_type in ["sl", "centroid"]:
+            return_vals["recobundles"][sl_type] = func(
+                dict_["recobundles"][sl_type],
+                *args, **kwargs)
+    if not dry_run:
+        for roi_type, roi in return_vals.items():
+            dict_[roi_type] = roi
+    return return_vals

AFQ/recognition/roi.py
@@ -1,6 +1,9 @@
 import numpy as np
-from scipy.spatial.distance import cdist
-from scipy.ndimage import binary_dilation
+from dipy.core.interpolation import interpolate_scalar_3d
+
+
+def _interp3d(roi, sl):
+    return interpolate_scalar_3d(roi.get_fdata(), np.asarray(sl))[0]
 
 
 def check_sls_with_inclusion(sls, include_rois, include_roi_tols):
@@ -17,15 +20,16 @@ def check_sl_with_inclusion(sl, include_rois,
     Helper function to check that a streamline is close to a list of
     inclusion ROIS.
     """
-    dist = []
+    closest = np.zeros(len(include_rois), dtype=np.int32)
     for ii, roi in enumerate(include_rois):
-        # Use squared Euclidean distance, because it's faster:
-        dist.append(cdist(sl, roi, 'sqeuclidean'))
-        if np.min(dist[-1]) > include_roi_tols[ii]:
+        dist = _interp3d(roi, sl)
+        closest[ii] = np.argmin(dist)
+        if dist[closest[ii]] > include_roi_tols[ii]:
             # Too far from one of them:
             return False, []
+
     # Apparently you checked all the ROIs and it was close to all of them
-    return True, dist
+    return True, closest
 
 
 def check_sl_with_exclusion(sl, exclude_rois,
@@ -34,8 +38,9 @@ def check_sl_with_exclusion(sl, exclude_rois,
     list of exclusion ROIs.
     """
     for ii, roi in enumerate(exclude_rois):
-        # Use squared Euclidean distance, because it's faster:
-        if np.min(cdist(sl, roi, 'sqeuclidean')) < exclude_roi_tols[ii]:
+        # if any part of the streamline is near any exclusion ROI,
+        # return False
+        if np.any(_interp3d(roi, sl) <= exclude_roi_tols[ii]):
             return False
     # Either there are no exclusion ROIs, or you are not close to any:
     return True
@@ -78,17 +83,10 @@ def clean_by_endpoints(streamlines, target, target_idx, tol=0,
         flip_sls = np.zeros(len(streamlines))
     flip_sls = flip_sls.astype(int)
 
-    roi = target.get_fdata()
-    if tol > 0:
-        roi = binary_dilation(
-            roi,
-            iterations=tol)
-
     for ii, sl in enumerate(streamlines):
         this_idx = target_idx
         if flip_sls[ii]:
             this_idx = (len(sl) - this_idx - 1) % len(sl)
-        xx, yy, zz = sl[this_idx].astype(int)
-        accepted_idxs[ii] = roi[xx, yy, zz]
+        accepted_idxs[ii] = _interp3d(target, [sl[this_idx]])[0] <= tol
 
     return accepted_idxs