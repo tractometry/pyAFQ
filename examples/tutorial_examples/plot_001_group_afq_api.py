6e810497e53ac4b3eb7d0c9611d3ab8d4b8f08c3
36000 <jk6.28@outlook.com>
2025-08-05 14:30:42 -0700

    single thread some examples

--- examples/tutorial_examples/plot_001_group_afq_api.py
+++ examples/tutorial_examples/plot_001_group_afq_api.py
@@ -88,13 +88,15 @@ tracking_params = dict(n_seeds=25000,
 # We will also be using plotly to generate an interactive visualization.
 # The value `plotly_no_gif` indicates that interactive visualizations will be
 # generated as html web-pages that can be opened in a browser, but not as
-# static gif files.
+# static gif files. We set ray_num_cpus=1 to avoid memory issues running this
+# example on servers.
 
 myafq = GroupAFQ(
     bids_path=op.join(afd.afq_home, 'stanford_hardi'),
     preproc_pipeline='vistasoft',
     t1_pipeline='freesurfer',
     tracking_params=tracking_params,
+    ray_num_cpus=1,
     viz_backend_spec='plotly_no_gif')
 
 ##########################################################################
