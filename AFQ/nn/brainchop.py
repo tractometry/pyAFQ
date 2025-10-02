6c0dd695751e822244363a87a3e070533c1a8a93
36000 <jk6.28@outlook.com>
2025-10-02 13:25:17 -0700

    display full brainchop errors

--- AFQ/nn/brainchop.py
+++ AFQ/nn/brainchop.py
@@ -19,6 +19,37 @@ import logging
 logger = logging.getLogger('AFQ')
 
 
+def _brainchop_reslice(tmp_t1_file, tmp_out_file, output_dtype, full_input):
+    cmd = [
+        "niimath", "-", "-reslice_nn", tmp_t1_file,
+        "-gz", "1", tmp_out_file, "-odt", output_dtype]
+
+    subprocess.run(cmd, input=full_input, check=True)
+
+
+def _run_brainchop_command(func, args):
+    """
+    Run a Brainchop command line interface
+    with the provided arguments, but with error handling.
+    """
+    try:
+        return func(*args)
+    except subprocess.CalledProcessError as e:
+        logger.error("Command failed: %s", e.cmd)
+        logger.error("Return code: %s", e.returncode)
+        if e.stdout:
+            if isinstance(e.stdout, bytes):
+                logger.error("STDOUT:\n%s", e.stdout.decode())
+            else:
+                logger.error("STDOUT:\n%s", e.stdout)
+        if e.stderr:
+            if isinstance(e.stderr, bytes):
+                logger.error("STDERR:\n%s", e.stderr.decode())
+            else:
+                logger.error("STDERR:\n%s", e.stderr)
+        raise
+
+
 def run_brainchop(t1_img, model):
     """
     Run the Brainchop command line interface with the provided arguments.
@@ -34,14 +65,14 @@ def run_brainchop(t1_img, model):
         t1_data = np.clip(t1_data, 0, t1_data.max())
         nib.save(nib.Nifti1Image(t1_data, t1_img.affine), tmp_t1_file)
 
-        volume, header = conform(tmp_t1_file)
+        volume, header = _run_brainchop_command(conform, [tmp_t1_file])
 
         image = Tensor(volume.transpose((2, 1, 0)).astype(np.float32)).rearrange(
             "... -> 1 1 ..."
         )
 
         try:
-            output_channels = model(image)
+            output_channels = _run_brainchop_command(model, [image])
         except Exception as e:
             if "clang" in str(e).lower():
                 with Context(PYTHON=1):
@@ -56,14 +87,20 @@ def run_brainchop(t1_img, model):
             .astype(np.uint8)
         )
 
-        labels, new_header = bwlabel(header, output)
+        labels, new_header = _run_brainchop_command(
+            bwlabel,
+            [header, output])
         full_input = new_header + labels.tobytes()
 
-        cmd = [
-            "niimath", "-", "-reslice_nn", tmp_t1_file,
-            "-gz", "1", tmp_out_file, "-odt", output_dtype]
-
-        subprocess.run(cmd, input=full_input, check=True)
+        _run_brainchop_command(
+            _brainchop_reslice,
+            [
+                tmp_t1_file,
+                tmp_out_file,
+                output_dtype,
+                full_input
+            ]
+        )
 
         output_img = nib.load(tmp_out_file)
         # This line below forces the data into memory to avoid issues with
