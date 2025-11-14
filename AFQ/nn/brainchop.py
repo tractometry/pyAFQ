217128c0a128d3fdb3feb33b7cb03d445a54a2e9
36000 <jk6.28@outlook.com>
2025-11-14 00:14:51 -0800

    no more niimath

--- AFQ/nn/brainchop.py
+++ AFQ/nn/brainchop.py
@@ -1,30 +1,19 @@
 import subprocess
-import tempfile
-import os
 
 import numpy as np
 import nibabel as nib
+import nibabel.processing as nbp
 
-from brainchop.utils import get_model
-from brainchop.niimath import (
-    conform,
-    bwlabel)
-
-from tinygrad import Tensor
-from tinygrad.helpers import Context
+from skimage.measure import label
 
 import logging
 
+from brainchop.utils import get_model
+
+from tinygrad import Tensor
+
 
 logger = logging.getLogger('AFQ')
-
-
-def _brainchop_reslice(tmp_t1_file, tmp_out_file, output_dtype, full_input):
-    cmd = [
-        "niimath", "-", "-reslice_nn", tmp_t1_file,
-        "-gz", "1", tmp_out_file, "-odt", output_dtype]
-
-    subprocess.run(cmd, input=full_input, check=True)
 
 
 def _run_brainchop_command(func, args):
@@ -50,66 +39,45 @@ def _run_brainchop_command(func, args):
         raise
 
 
-def run_brainchop(t1_img, model):
+def run_brainchop(t1_img, model_name):
     """
     Run the Brainchop command line interface with the provided arguments.
     """
-    model = get_model(model)
-    output_dtype = "char"
+    model = get_model(model_name)
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        tmp_t1_file = f"{temp_dir}/t1.nii.gz"
-        tmp_out_file = f"{temp_dir}/output.nii.gz"
+    t1_img_conformed = nbp.conform(
+        t1_img,
+        out_shape=(256, 256, 256),
+        voxel_size=(1.0, 1.0, 1.0),
+        orientation="LIA")
 
-        t1_data = t1_img.get_fdata()
-        t1_data = np.clip(t1_data, 0, t1_data.max())
-        nib.save(nib.Nifti1Image(t1_data, t1_img.affine), tmp_t1_file)
+    t1_data = t1_img_conformed.get_fdata()
+    p02 = np.nanpercentile(t1_data, 2)
+    p98 = np.nanpercentile(t1_data, 98)
+    t1_data = np.clip(t1_data, p02, p98)
+    t1_data = (t1_data - p02) / (p98 - p02)
+    t1_data = (t1_data * 255.0)
+    
+    image = Tensor(t1_data.astype(np.float32)).rearrange(
+        "... -> 1 1 ..."
+    )
 
-        volume, header = _run_brainchop_command(conform, [tmp_t1_file])
+    logger.info(f"Running {model_name}...")
+    output_channels = _run_brainchop_command(model, [image])
 
-        image = Tensor(volume.transpose((2, 1, 0)).astype(np.float32)).rearrange(
-            "... -> 1 1 ..."
-        )
+    output = (
+        output_channels.argmax(axis=1)
+        .squeeze(0)
+        .numpy()
+        .astype(np.uint8)
+    )
+    
+    output = label(output, background=0)
 
-        try:
-            output_channels = _run_brainchop_command(model, [image])
-        except Exception as e:
-            if "clang" in str(e).lower():
-                with Context(PYTHON=1):
-                    output_channels = model(image)
-            else:
-                raise
-
-        output = (
-            output_channels.argmax(axis=1)
-            .rearrange("1 x y z -> z y x")
-            .numpy()
-            .astype(np.uint8)
-        )
-
-        labels, new_header = _run_brainchop_command(
-            bwlabel,
-            [header, output])
-        full_input = new_header + labels.tobytes()
-
-        _run_brainchop_command(
-            _brainchop_reslice,
-            [
-                tmp_t1_file,
-                tmp_out_file,
-                output_dtype,
-                full_input
-            ]
-        )
-
-        output_img = nib.load(tmp_out_file)
-        # This line below forces the data into memory to avoid issues with
-        # temporary files being deleted too early.
-        # Otherwise, nibabel lazy-loads the data and the file gets deleted
-        # before the data is accessed, because the file is in a temporary
-        # Directory.
-        output_img = nib.Nifti1Image(
-            output_img.get_fdata().copy(),
-            output_img.affine.copy())
+    output_img = nbp.resample_from_to(
+        nib.Nifti1Image(
+            output.astype(np.float32),
+            t1_img_conformed.affine),
+        t1_img)
 
     return output_img
