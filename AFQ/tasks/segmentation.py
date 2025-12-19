e002923fd397303a94da2c6dd9036572832800a2
Ariel Rokem <arokem@gmail.com>
2025-12-19 14:40:50 -0800

    Linting in tasks/segmentation.py

--- AFQ/tasks/segmentation.py
+++ AFQ/tasks/segmentation.py
@@ -19,6 +19,7 @@ try:
     from trx.io import load as load_trx
     from trx.io import save as save_trx
     from trx.trx_file_memmap import TrxFile
+
     has_trx = True
 except ModuleNotFoundError:
     has_trx = False
@@ -34,13 +35,12 @@ from dipy.tracking.streamline import set_number_of_points, values_from_volume
 from nibabel.affines import voxel_sizes
 from nibabel.orientations import aff2axcodes
 
-logger = logging.getLogger('AFQ')
+logger = logging.getLogger("AFQ")
 
 
 @immlib.calc("bundles")
-@as_file('_desc-bundles_tractography')
-def segment(data_imap, mapping_imap,
-            tractography_imap, segmentation_params):
+@as_file("_desc-bundles_tractography")
+def segment(data_imap, mapping_imap, tractography_imap, segmentation_params):
     """
     full path to a trk/trx file containing containing
     segmented streamlines, labeled by bundle
@@ -54,12 +54,14 @@ def segment(data_imap, mapping_imap,
     bundle_dict = data_imap["bundle_dict"]
     reg_template = data_imap["reg_template"]
     streamlines = tractography_imap["streamlines"]
-    if streamlines.endswith(".trk") or\
-        streamlines.endswith(".tck") or\
-            streamlines.endswith(".vtk"):
+    if (
+        streamlines.endswith(".trk")
+        or streamlines.endswith(".tck")
+        or streamlines.endswith(".vtk")
+    ):
         tg = load_tractogram(
-            streamlines, data_imap["dwi"], Space.VOX,
-            bbox_valid_check=False)
+            streamlines, data_imap["dwi"], Space.VOX, bbox_valid_check=False
+        )
         is_trx = False
     elif streamlines.endswith(".trx"):
         is_trx = True
@@ -73,36 +75,32 @@ def segment(data_imap, mapping_imap,
         space_attributes = (affine, dimensions, vox_sizes, vox_order)
 
         # Avoid deep copy triggered by to_sft
-        tg = StatefulTractogram(
-            trx.streamlines,
-            space_attributes,
-            Space.RASMM)
+        tg = StatefulTractogram(trx.streamlines, space_attributes, Space.RASMM)
         del trx
     elif streamlines.endswith(".tck.gz"):
         # uncompress tck.gz to a temporary tck:
-        temp_tck = op.join(mkdtemp(), op.split(
-            streamlines.replace(".gz", ""))[1])
+        temp_tck = op.join(mkdtemp(), op.split(streamlines.replace(".gz", ""))[1])
         logger.info(f"Temporary tck file created at: {temp_tck}")
-        with gzip.open(streamlines, 'rb') as f_in:
-            with open(temp_tck, 'wb') as f_out:
+        with gzip.open(streamlines, "rb") as f_in:
+            with open(temp_tck, "wb") as f_out:
                 shutil.copyfileobj(f_in, f_out)
         # initialize stateful tractogram from tck file:
         tg = load_tractogram(
-            temp_tck, data_imap["dwi"], Space.VOX,
-            bbox_valid_check=False)
+            temp_tck, data_imap["dwi"], Space.VOX, bbox_valid_check=False
+        )
         is_trx = False
     if len(tg.streamlines) == 0:
-        raise ValueError(f"There are no streamlines in {streamlines}."
-                         " This is likely due to errors in defining the "
-                         " tractography parameters or the"
-                         " seed/PVE masks.")
+        raise ValueError(
+            f"There are no streamlines in {streamlines}."
+            " This is likely due to errors in defining the "
+            " tractography parameters or the"
+            " seed/PVE masks."
+        )
 
     if not is_trx:
         indices_to_remove, _ = tg.remove_invalid_streamlines()
         if len(indices_to_remove) > 0:
-            logger.warning((
-                f"{len(indices_to_remove)} invalid "
-                "streamlines removed"))
+            logger.warning((f"{len(indices_to_remove)} invalid streamlines removed"))
 
     start_time = time()
     bundles, bundle_meta = recognize(
@@ -112,7 +110,8 @@ def segment(data_imap, mapping_imap,
         bundle_dict,
         reg_template,
         data_imap["n_cpus"],
-        **segmentation_params)
+        **segmentation_params,
+    )
 
     seg_sft = aus.SegmentedSFT(bundles, Space.VOX)
 
@@ -120,8 +119,7 @@ def segment(data_imap, mapping_imap,
         raise ValueError("Fatal: No bundles recognized.")
 
     if is_trx:
-        seg_sft.sft.dtype_dict = {'positions': np.float16,
-                                  'offsets': np.uint32}
+        seg_sft.sft.dtype_dict = {"positions": np.float16, "offsets": np.uint32}
         tgram = TrxFile.from_sft(seg_sft.sft)
         tgram.groups = seg_sft.bundle_idxs
 
@@ -150,9 +148,7 @@ def segment(data_imap, mapping_imap,
 
 
 @immlib.calc("indiv_bundles")
-def export_bundles(base_fname, output_dir,
-                   bundles,
-                   tracking_params):
+def export_bundles(base_fname, output_dir, bundles, tracking_params):
     """
     dictionary of paths, where each path is
     a full path to a trk file containing the streamlines of a given bundle.
@@ -168,40 +164,34 @@ def export_bundles(base_fname, output_dir,
     for bundle in seg_sft.bundle_names:
         fname = get_fname(
             base_fname,
-            f'_desc-{str_to_desc(bundle)}'
-            f'_tractography{extension}',
-            subfolder="bundles")
+            f"_desc-{str_to_desc(bundle)}_tractography{extension}",
+            subfolder="bundles",
+        )
         if op.exists(fname):
-            logger.info(
-                f"Bundle {bundle} already exists at {fname}. "
-                "Skipping export.")
+            logger.info(f"Bundle {bundle} already exists at {fname}. Skipping export.")
         else:
             bundle_sft = seg_sft.get_bundle(bundle)
             if len(bundle_sft) > 0:
                 logger.info(f"Saving {fname}")
                 if is_trx:
                     seg_sft.sft.dtype_dict = {
-                        'positions': np.float16,
-                        'offsets': np.uint32}
+                        "positions": np.float16,
+                        "offsets": np.uint32,
+                    }
                     trxfile = TrxFile.from_sft(bundle_sft)
                     save_trx(trxfile, fname)
                 else:
-                    save_tractogram(
-                        bundle_sft, fname,
-                        bbox_valid_check=False)
+                    save_tractogram(bundle_sft, fname, bbox_valid_check=False)
             else:
                 logger.info(f"No bundle to save for {bundle}")
-            meta = dict(
-                source=bundles,
-                params=seg_sft.get_bundle_param_info(bundle))
-            meta_fname = drop_extension(fname) + '.json'
+            meta = {"source": bundles, "params": seg_sft.get_bundle_param_info(bundle)}
+            meta_fname = drop_extension(fname) + ".json"
             write_json(meta_fname, meta)
     return op.dirname(fname)
 
 
 @immlib.calc("sl_counts")
-@as_file('_desc-slCount_tractography.csv',
-         subfolder="stats")
+@as_file("_desc-slCount_tractography.csv", subfolder="stats")
 def export_sl_counts(bundles):
     """
     full path to a JSON file containing streamline counts
@@ -210,21 +200,18 @@ def export_sl_counts(bundles):
     seg_sft = aus.SegmentedSFT.fromfile(bundles)
 
     for bundle in seg_sft.bundle_names:
-        sl_counts.append(len(
-            seg_sft.get_bundle(bundle).streamlines))
+        sl_counts.append(len(seg_sft.get_bundle(bundle).streamlines))
     sl_counts.append(len(seg_sft.sft.streamlines))
 
     counts_df = pd.DataFrame(
-        data=dict(
-            n_streamlines=sl_counts),
-        index=seg_sft.bundle_names + ["Total Recognized"])
-    return counts_df, dict(source=bundles)
+        data={"n_streamlines": sl_counts},
+        index=seg_sft.bundle_names + ["Total Recognized"],
+    )
+    return counts_df, {"source": bundles}
 
 
 @immlib.calc("median_bundle_lengths")
-@as_file(
-    '_desc-medianBundleLengths_tractography.csv',
-    subfolder="stats")
+@as_file("_desc-medianBundleLengths_tractography.csv", subfolder="stats")
 def export_bundle_lengths(bundles):
     """
     full path to a JSON file containing median bundle lengths
@@ -233,51 +220,47 @@ def export_bundle_lengths(bundles):
     seg_sft = aus.SegmentedSFT.fromfile(bundles)
 
     for bundle in seg_sft.bundle_names:
-        these_lengths = seg_sft.get_bundle(
-            bundle)._tractogram._streamlines._lengths
+        these_lengths = seg_sft.get_bundle(bundle)._tractogram._streamlines._lengths
         if len(these_lengths) > 0:
-            med_len_counts.append(np.median(
-                these_lengths))
+            med_len_counts.append(np.median(these_lengths))
         else:
             med_len_counts.append(0)
-    med_len_counts.append(np.median(
-        seg_sft.sft._tractogram._streamlines._lengths))
+    med_len_counts.append(np.median(seg_sft.sft._tractogram._streamlines._lengths))
 
     counts_df = pd.DataFrame(
-        data=dict(
-            median_len=med_len_counts),
-        index=seg_sft.bundle_names + ["Total Recognized"])
-    return counts_df, dict(source=bundles)
+        data={"median_len": med_len_counts},
+        index=seg_sft.bundle_names + ["Total Recognized"],
+    )
+    return counts_df, {"source": bundles}
 
 
 @immlib.calc("density_maps")
-@as_file('_desc-density_tractography.nii.gz')
+@as_file("_desc-density_tractography.nii.gz")
 def export_density_maps(bundles, data_imap):
     """
     full path to 4d nifti file containing streamline counts per voxel
     per bundle, where the 4th dimension encodes the bundle
     """
-    seg_sft = aus.SegmentedSFT.fromfile(
-        bundles)
-    entire_density_map = np.zeros((
-        *data_imap["data"].shape[:3],
-        len(seg_sft.bundle_names)))
+    seg_sft = aus.SegmentedSFT.fromfile(bundles)
+    entire_density_map = np.zeros(
+        (*data_imap["data"].shape[:3], len(seg_sft.bundle_names))
+    )
     for ii, bundle_name in enumerate(seg_sft.bundle_names):
         bundle_sl = seg_sft.get_bundle(bundle_name)
         bundle_density = auv.density_map(bundle_sl).get_fdata()
         entire_density_map[..., ii] = bundle_density
 
-    return nib.Nifti1Image(
-        entire_density_map, data_imap["dwi_affine"]), dict(
-            source=bundles, bundles=list(seg_sft.bundle_names))
+    return (
+        nib.Nifti1Image(entire_density_map, data_imap["dwi_affine"]),
+        {"source": bundles, "bundles": list(seg_sft.bundle_names)},
+    )
 
 
 @immlib.calc("profiles")
-@as_file('_desc-profiles_tractography.csv')
-def tract_profiles(bundles,
-                   scalar_dict, dwi_affine,
-                   profile_weights="gauss",
-                   n_points_profile=100):
+@as_file("_desc-profiles_tractography.csv")
+def tract_profiles(
+    bundles, scalar_dict, dwi_affine, profile_weights="gauss", n_points_profile=100
+):
     """
     full path to a CSV file containing tract profiles
 
@@ -296,29 +279,32 @@ def tract_profiles(bundles,
         calculating the tract-profiles.
         Default: 100
     """
-    if not (profile_weights is None
-            or isinstance(profile_weights, str)
-            or callable(profile_weights)
-            or hasattr(profile_weights, "__len__")):
+    if not (
+        profile_weights is None
+        or isinstance(profile_weights, str)
+        or callable(profile_weights)
+        or hasattr(profile_weights, "__len__")
+    ):
         raise TypeError(
-            "profile_weights must be string, None, callable, or"
-            + "a 1D or 2D array")
+            "profile_weights must be string, None, callable, or" + "a 1D or 2D array"
+        )
     if isinstance(profile_weights, str):
         profile_weights = profile_weights.lower()
-    if isinstance(profile_weights, str) and\
-            profile_weights != "gauss" and profile_weights != "median":
+    if (
+        isinstance(profile_weights, str)
+        and profile_weights != "gauss"
+        and profile_weights != "median"
+    ):
         raise TypeError(
-            "if profile_weights is a string,"
-            + " it must be 'gauss' or 'median'")
+            "if profile_weights is a string," + " it must be 'gauss' or 'median'"
+        )
 
     bundle_names = []
     node_numbers = []
     profiles = np.empty((len(scalar_dict), 0)).tolist()
     this_profile = np.zeros((len(scalar_dict), n_points_profile))
     reference = nib.load(scalar_dict[list(scalar_dict.keys())[0]])
-    seg_sft = aus.SegmentedSFT.fromfile(
-        bundles,
-        reference=reference)
+    seg_sft = aus.SegmentedSFT.fromfile(bundles, reference=reference)
 
     seg_sft.sft.to_rasmm()
     for bundle_name in seg_sft.bundle_names:
@@ -328,8 +314,8 @@ def tract_profiles(bundles,
         if profile_weights == "gauss":
             # calculate only once per bundle
             bundle_profile_weights = gaussian_weights(
-                this_sl,
-                n_points=n_points_profile)
+                this_sl, n_points=n_points_profile
+            )
         for ii, (scalar, scalar_file) in enumerate(scalar_dict.items()):
             if isinstance(scalar_file, str):
                 scalar_file = nib.load(scalar_file)
@@ -340,64 +326,73 @@ def tract_profiles(bundles,
                 elif profile_weights == "median":
                     # weights bundle to only return the mean
                     def _median_weight(bundle):
-                        fgarray = set_number_of_points(
-                            bundle, n_points_profile)
+                        fgarray = set_number_of_points(bundle, n_points_profile)
                         values = np.array(
-                            values_from_volume(
-                                scalar_data,
-                                fgarray,
-                                dwi_affine))
+                            values_from_volume(scalar_data, fgarray, dwi_affine)  # noqa B023
+                        )
                         weights = np.zeros(values.shape)
                         for ii, jj in enumerate(
-                            np.argsort(values, axis=0)[
-                                len(values) // 2, :]):
+                            np.argsort(values, axis=0)[len(values) // 2, :]
+                        ):
                             weights[jj, ii] = 1
                         return weights
+
                     this_prof_weights = _median_weight
             else:
                 this_prof_weights = np.asarray(profile_weights)
-            if isinstance(this_prof_weights, np.ndarray) and \
-                    np.any(np.isnan(this_prof_weights)):  # fit failed
-                logger.warning((
-                    f"Even weighting used for "
-                    f"bundle {bundle_name}, scalar {scalar} "
-                    f"in profiling due inability to estimate weights. "
-                    "This is often caused by low streamline count or "
-                    "low variance in the scalar data."))
+            if isinstance(this_prof_weights, np.ndarray) and np.any(
+                np.isnan(this_prof_weights)
+            ):  # fit failed
+                logger.warning(
+                    (
+                        f"Even weighting used for "
+                        f"bundle {bundle_name}, scalar {scalar} "
+                        f"in profiling due inability to estimate weights. "
+                        "This is often caused by low streamline count or "
+                        "low variance in the scalar data."
+                    )
+                )
                 this_prof_weights = np.ones_like(this_prof_weights)
             this_profile[ii] = afq_profile(
                 scalar_data,
                 this_sl,
                 dwi_affine,
                 weights=this_prof_weights,
-                n_points=n_points_profile)
+                n_points=n_points_profile,
+            )
             profiles[ii].extend(list(this_profile[ii]))
         nodes = list(np.arange(this_profile[0].shape[0]))
         bundle_names.extend([bundle_name] * len(nodes))
         node_numbers.extend(nodes)
 
-    profile_dict = dict()
+    profile_dict = {}
     profile_dict["tractID"] = bundle_names
     profile_dict["nodeID"] = node_numbers
     for ii, scalar in enumerate(scalar_dict.keys()):
         profile_dict[scalar] = profiles[ii]
 
     profile_dframe = pd.DataFrame(profile_dict)
-    meta = dict(source=bundles,
-                parameters=get_default_args(afq_profile),
-                scalars=list(scalar_dict.keys()),
-                bundles=list(seg_sft.bundle_names))
+    meta = {
+        "source": bundles,
+        "parameters": get_default_args(afq_profile),
+        "scalars": list(scalar_dict.keys()),
+        "bundles": list(seg_sft.bundle_names),
+    }
 
     return profile_dframe, meta
 
 
 @immlib.calc("scalar_dict")
-def get_scalar_dict(structural_imap, data_imap, tissue_imap,
-                    mapping_imap,
-                    t1_file,
-                    scalars=["dti_fa", "dti_md", "t1w"]):
+def get_scalar_dict(
+    structural_imap,
+    data_imap,
+    tissue_imap,
+    mapping_imap,
+    t1_file,
+    scalars=None,
+):
     """
-    dicionary mapping scalar names
+    dictionary mapping scalar names
     to their respective file paths
 
     Parameters
@@ -413,6 +408,8 @@ def get_scalar_dict(structural_imap, data_imap, tissue_imap,
         "dki_mk", "t1w"].
         Default: ['dti_fa', 'dti_md', 't1w']
     """
+    if scalars is None:
+        scalars = ["dti_fa", "dti_md", "t1w"]
     # Note: some scalars preprocessing done in mapping plan, before this step
     scalar_dict = {}
     for scalar in scalars:
@@ -421,33 +418,34 @@ def get_scalar_dict(structural_imap, data_imap, tissue_imap,
             if sc == "t1w":
                 scalar_dict[sc] = t1_file
             else:
-                scalar_dict[sc] = get_tp(f"{sc}",
-                                         structural_imap,
-                                         data_imap,
-                                         tissue_imap)
+                scalar_dict[sc] = get_tp(
+                    f"{sc}", structural_imap, data_imap, tissue_imap
+                )
         elif f"{scalar.get_name()}" in mapping_imap:
-            scalar_dict[scalar.get_name()] = mapping_imap[
-                f"{scalar.get_name()}"]
+            scalar_dict[scalar.get_name()] = mapping_imap[f"{scalar.get_name()}"]
     return {"scalar_dict": scalar_dict}
 
 
 def get_segmentation_plan(kwargs):
-    if "segmentation_params" in kwargs\
-            and not isinstance(kwargs["segmentation_params"], dict):
-        raise TypeError(
-            "segmentation_params a dict")
+    if "segmentation_params" in kwargs and not isinstance(
+        kwargs["segmentation_params"], dict
+    ):
+        raise TypeError("segmentation_params a dict")
     if "cleaning_params" in kwargs:
         raise ValueError(
-            "cleaning_params should be passed inside of"
-            "segmentation_params")
-    segmentation_tasks = with_name([
-        get_scalar_dict,
-        export_sl_counts,
-        export_bundle_lengths,
-        export_bundles,
-        export_density_maps,
-        segment,
-        tract_profiles])
+            "cleaning_params should be passed inside ofsegmentation_params"
+        )
+    segmentation_tasks = with_name(
+        [
+            get_scalar_dict,
+            export_sl_counts,
+            export_bundle_lengths,
+            export_bundles,
+            export_density_maps,
+            segment,
+            tract_profiles,
+        ]
+    )
 
     default_seg_params = get_default_args(recognize)
     if "segmentation_params" in kwargs:
