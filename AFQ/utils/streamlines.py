0b64f5d4a503cc4f71aeeff0e397e7365e4a4635
Ariel Rokem <arokem@gmail.com>
2025-12-19 15:26:38 -0800

    Linting in AFQ/utils/streamlines.

--- AFQ/utils/streamlines.py
+++ AFQ/utils/streamlines.py
@@ -6,6 +6,7 @@ from dipy.io.streamline import load_tractogram
 
 try:
     from trx.io import load as load_trx
+
     has_trx = True
 except ModuleNotFoundError:
     has_trx = False
@@ -13,8 +14,8 @@ except ModuleNotFoundError:
 from AFQ.utils.path import drop_extension, read_json
 
 
-class SegmentedSFT():
-    def __init__(self, bundles, space, sidecar_info={}):
+class SegmentedSFT:
+    def __init__(self, bundles, space, sidecar_info=None):
         reference = None
         self.bundle_names = []
         sls = []
@@ -23,8 +24,8 @@ class SegmentedSFT():
         idx_count = 0
         for b_name in bundles:
             if isinstance(bundles[b_name], dict):
-                this_sls = bundles[b_name]['sl']
-                this_tracking_idxs[b_name] = bundles[b_name]['idx']
+                this_sls = bundles[b_name]["sl"]
+                this_tracking_idxs[b_name] = bundles[b_name]["idx"]
             else:
                 this_sls = bundles[b_name]
             if reference is None:
@@ -42,7 +43,8 @@ class SegmentedSFT():
             self.this_tracking_idxs = this_tracking_idxs
         else:
             self.this_tracking_idxs = None
-
+        if sidecar_info is None:
+            sidecar_info = {}
         self.sidecar_info = sidecar_info
         self.sidecar_info["bundle_ids"] = {}
         dps = np.zeros(len(self.sft.streamlines))
@@ -52,28 +54,31 @@ class SegmentedSFT():
         dps = {"bundle": dps}
         self.sft.data_per_streamline = dps
         if self.this_tracking_idxs is not None:
-            for kk, vv in self.this_tracking_idxs.items():
-                self.this_tracking_idxs[kk] =\
+            for kk, _ in self.this_tracking_idxs.items():
+                self.this_tracking_idxs[kk] = (
                     self.this_tracking_idxs[kk].astype(int).tolist()
+                )
             self.sidecar_info["tracking_idx"] = self.this_tracking_idxs
 
     def get_bundle(self, b_name):
         return self.sft[self.bundle_idxs[b_name]]
 
     def get_bundle_param_info(self, b_name):
-        return self.sidecar_info.get(
-            "Bundle Parameters", {}).get(b_name, {})
+        return self.sidecar_info.get("Bundle Parameters", {}).get(b_name, {})
 
     @classmethod
     def fromfile(cls, trk_or_trx_file, reference="same", sidecar_file=None):
         if sidecar_file is None:
             # assume json sidecar has the same name as trk_file,
             # but with json suffix
-            sidecar_file = f'{drop_extension(trk_or_trx_file)}.json'
+            sidecar_file = f"{drop_extension(trk_or_trx_file)}.json"
             if not op.exists(sidecar_file):
-                raise ValueError((
-                    "JSON sidecars are required for trk files. "
-                    f"JSON sidecar not found for: {sidecar_file}"))
+                raise ValueError(
+                    (
+                        "JSON sidecars are required for trk files. "
+                        f"JSON sidecar not found for: {sidecar_file}"
+                    )
+                )
         sidecar_info = read_json(sidecar_file)
         if trk_or_trx_file.endswith(".trx"):
             trx = load_trx(trk_or_trx_file, reference)
@@ -85,20 +90,20 @@ class SegmentedSFT():
             for bundle in trx.groups:
                 idx = trx.groups[bundle]
                 bundles[bundle] = StatefulTractogram(
-                    sft.streamlines[idx], reference, Space.RASMM)
+                    sft.streamlines[idx], reference, Space.RASMM
+                )
         else:
-            sft = load_tractogram(trk_or_trx_file, reference,
-                                  to_space=Space.RASMM)
+            sft = load_tractogram(trk_or_trx_file, reference, to_space=Space.RASMM)
 
             if reference == "same":
                 reference = sft
             bundles = {}
             if "bundle_ids" in sidecar_info:
                 for b_name, b_id in sidecar_info["bundle_ids"].items():
-                    idx = np.where(
-                        sft.data_per_streamline['bundle'] == b_id)[0]
+                    idx = np.where(sft.data_per_streamline["bundle"] == b_id)[0]
                     bundles[b_name] = StatefulTractogram(
-                        sft.streamlines[idx], reference, Space.RASMM)
+                        sft.streamlines[idx], reference, Space.RASMM
+                    )
             else:
                 bundles["whole_brain"] = sft
 
@@ -120,14 +125,17 @@ def split_streamline(streamlines, sl_to_split, split_idx):
     """
     this_sl = streamlines[sl_to_split]
 
-    streamlines._lengths = np.concatenate([
-        streamlines._lengths[:sl_to_split],
-        np.array([split_idx]),
-        np.array([this_sl.shape[0] - split_idx]),
-        streamlines._lengths[sl_to_split + 1:]])
+    streamlines._lengths = np.concatenate(
+        [
+            streamlines._lengths[:sl_to_split],
+            np.array([split_idx]),
+            np.array([this_sl.shape[0] - split_idx]),
+            streamlines._lengths[sl_to_split + 1 :],
+        ]
+    )
 
-    streamlines._offsets = np.concatenate([
-        np.array([0]),
-        np.cumsum(streamlines._lengths[:-1])])
+    streamlines._offsets = np.concatenate(
+        [np.array([0]), np.cumsum(streamlines._lengths[:-1])]
+    )
 
     return streamlines
