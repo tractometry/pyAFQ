bc9765a9c1c378cd13331e004323017c3e3c788e
Ariel Rokem <arokem@gmail.com>
2025-12-19 16:10:55 -0800

    Linting in one of the examples.

--- examples/howto_examples/add_custom_bundle.py
+++ examples/howto_examples/add_custom_bundle.py
@@ -38,50 +38,52 @@ np.random.seed(1234)
 # folder. These 12 subjects have very high quality data.
 # The fether returns this directory as study_dir:
 
-_, study_dir = afd.fetch_hbn_preproc([
-    'NDARKP893TWU',
-    'NDAREP505XAD',
-    'NDARKT540ZW0',
-    'NDARAG340ERT',
-    'NDAREM757NBG',
-    'NDARLL894HC3',
-    'NDARFY525TL2',
-    'NDARKV461KGZ',
-    'NDARUC851WHU',
-    'NDARMJ333WJM',
-    'NDARJG687YYX',
-    'NDARJA157YB3',
-])
+_, study_dir = afd.fetch_hbn_preproc(
+    [
+        "NDARKP893TWU",
+        "NDAREP505XAD",
+        "NDARKT540ZW0",
+        "NDARAG340ERT",
+        "NDAREM757NBG",
+        "NDARLL894HC3",
+        "NDARFY525TL2",
+        "NDARKV461KGZ",
+        "NDARUC851WHU",
+        "NDARMJ333WJM",
+        "NDARJG687YYX",
+        "NDARJA157YB3",
+    ]
+)
 
 #############################################################################
 # Get ROIs and save to disk
 # --------------------------------
-# The goal of this tutorial is to demostrate how to segment new pathways based
+# The goal of this tutorial is to demonstrate how to segment new pathways based
 # on ROIs that are saved to disk. In principle, ROIs can be a) files created by
 # the user and saved to the local disk, b) files stored somewhere on the internet
 # (as is the case here) or c) Files that are accessed with a fetcher. In this
 # example we download these files from the MATLAB AFQ website, but this code could
 # be commented out and paths could be used to local ROIs on disk
 
-roi_urls = ['https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/MFgL.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/MFgR.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PaL.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PaR.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PrgL.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PrgR.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SFgL.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SFgR.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SLFt_roi2_L.nii.gz',
-            'https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SLFt_roi2_R.nii.gz']
+roi_urls = [
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/MFgL.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/MFgR.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PaL.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PaR.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PrgL.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/PrgR.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SFgL.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SFgR.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SLFt_roi2_L.nii.gz",
+    "https://github.com/yeatmanlab/AFQ/raw/c762ca4c393f2105d4f444c44d9e4b4702f0a646/SLF123/ROIs/SLFt_roi2_R.nii.gz",
+]
 
 # We proceed to download the files. First, we define and create the directory
 # for the template ROIs. In the code below, ``op.expanduser("~")`` expands the
 # user's home directory into the full path and ``op.join`` joins these paths,
 # to make the path `~/AFQ_data/SLF_ROIs/`
 
-template_dir = op.join(
-    op.expanduser("~"),
-    'AFQ_data/SLF_ROIs/')
+template_dir = op.join(op.expanduser("~"), "AFQ_data/SLF_ROIs/")
 os.makedirs(template_dir, exist_ok=True)
 
 # The `wget` Python library works like the `wget` unix command and downloads
@@ -102,50 +104,40 @@ for roi_url in roi_urls:
 # to populate the `BundleDict`, but we could also provide already-loaded nifti
 # objects, as demonstrated in other examples.
 
-bundles = abd.BundleDict({
-    "L_SLF1": {
-        "include": [
-            template_dir + 'SFgL.nii.gz',
-            template_dir + 'PaL.nii.gz'],
-        "exclude": [
-            template_dir + 'SLFt_roi2_L.nii.gz'],
-
-        "cross_midline": False,
-
-        "mahal": {
-            "clean_rounds": 20,
-            "length_threshold": 4,
-            "distance_threshold": 2}
-    },
-    "L_SLF2": {
-        "include": [
-            template_dir + 'MFgL.nii.gz',
-            template_dir + 'PaL.nii.gz'],
-        "exclude": [
-            template_dir + 'SLFt_roi2_L.nii.gz'],
-
-        "cross_midline": False,
-
-        "mahal": {
-            "clean_rounds": 20,
-            "length_threshold": 4,
-            "distance_threshold": 2}
-    },
-    "L_SLF3": {
-        "include": [
-            template_dir + 'PrgL.nii.gz',
-            template_dir + 'PaL.nii.gz'],
-        "exclude": [
-            template_dir + 'SLFt_roi2_L.nii.gz'],
-
-        "cross_midline": False,
-
-        "mahal": {
-            "clean_rounds": 20,
-            "length_threshold": 4,
-            "distance_threshold": 2}
+bundles = abd.BundleDict(
+    {
+        "L_SLF1": {
+            "include": [template_dir + "SFgL.nii.gz", template_dir + "PaL.nii.gz"],
+            "exclude": [template_dir + "SLFt_roi2_L.nii.gz"],
+            "cross_midline": False,
+            "mahal": {
+                "clean_rounds": 20,
+                "length_threshold": 4,
+                "distance_threshold": 2,
+            },
+        },
+        "L_SLF2": {
+            "include": [template_dir + "MFgL.nii.gz", template_dir + "PaL.nii.gz"],
+            "exclude": [template_dir + "SLFt_roi2_L.nii.gz"],
+            "cross_midline": False,
+            "mahal": {
+                "clean_rounds": 20,
+                "length_threshold": 4,
+                "distance_threshold": 2,
+            },
+        },
+        "L_SLF3": {
+            "include": [template_dir + "PrgL.nii.gz", template_dir + "PaL.nii.gz"],
+            "exclude": [template_dir + "SLFt_roi2_L.nii.gz"],
+            "cross_midline": False,
+            "mahal": {
+                "clean_rounds": 20,
+                "length_threshold": 4,
+                "distance_threshold": 2,
+            },
+        },
     }
-})
+)
 
 #############################################################################
 # Custom bundle definitions such as the SLF or OR, and the standard BundleDict
@@ -173,11 +165,14 @@ my_afq = GroupAFQ(
     bids_path=study_dir,
     dwi_preproc_pipeline="qsiprep",
     output_dir=op.join(study_dir, "derivatives", "afq_slf"),
-    tracking_params={"n_seeds": 200000,
-                     "directions": "pft",
-                     "odf_model": "CSD",
-                     "seed_mask": RoiImage()},
-    bundle_info=bundles)
+    tracking_params={
+        "n_seeds": 200000,
+        "directions": "pft",
+        "odf_model": "CSD",
+        "seed_mask": RoiImage(),
+    },
+    bundle_info=bundles,
+)
 
 # If you want to redo different stages you can use the `clobber` method.
 # The options for dependent_on are 'track' (to start over from tractography)
@@ -186,7 +181,7 @@ my_afq = GroupAFQ(
 # This is useful when changing something about how the bundles are recognized.
 # For example, the cleaning parameters.
 
-my_afq.clobber(dependent_on='recog')
+my_afq.clobber(dependent_on="recog")
 
 my_afq.export_all()
 
@@ -194,16 +189,14 @@ my_afq.export_all()
 # Visualize a montage
 # ----------------------
 # One way to examine the output of the pyAFQ pipeline is by creating a montage
-# of images of a particular bundle across a group of participants. In the montage function
-# the first input refers to a key in the bundlediect and the second gives the layout
-# of the figure (eg. 3 rows 4 columns) and finally is the view.
+# of images of a particular bundle across a group of participants. In the
+# montage function the first input refers to a key in the bundlediect and the
+# second gives the layout of the figure (eg. 3 rows 4 columns) and finally is
+# the view.
 
-montage = my_afq.group_montage(
-    "L_SLF1", (3, 4), "Sagittal", "left", slice_pos=0.5)
-montage = my_afq.group_montage(
-    "L_SLF2", (3, 4), "Sagittal", "left", slice_pos=0.5)
-montage = my_afq.group_montage(
-    "L_SLF3", (3, 4), "Sagittal", "left", slice_pos=0.5)
+montage = my_afq.group_montage("L_SLF1", (3, 4), "Sagittal", "left", slice_pos=0.5)
+montage = my_afq.group_montage("L_SLF2", (3, 4), "Sagittal", "left", slice_pos=0.5)
+montage = my_afq.group_montage("L_SLF3", (3, 4), "Sagittal", "left", slice_pos=0.5)
 
 #############################################################################
 # Interactive bundle visualization
