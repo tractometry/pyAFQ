1a9ef55f220065d8e041ee234a010834a133680e
36000 <jk6.28@outlook.com>
2026-01-07 12:59:19 -0800

    update pyAFQ to use cuda python

--- AFQ/tractography/gputractography.py
+++ AFQ/tractography/gputractography.py
@@ -1,17 +1,16 @@
 import logging
 from math import radians
 
-import cuslines
 import nibabel as nib
 import numpy as np
+from cuslines import (
+    BootDirectionGetter,
+    GPUTracker,
+    ProbDirectionGetter,
+    PttDirectionGetter,
+)
 from dipy.align import resample
-from dipy.io.stateful_tractogram import Space, StatefulTractogram
 from dipy.reconst import shm
-from dipy.reconst.shm import CsaOdfModel, OpdtModel
-from nibabel.streamlines.array_sequence import concatenate
-from nibabel.streamlines.tractogram import Tractogram
-from tqdm import tqdm
-from trx.trx_file_memmap import TrxFile
 
 from AFQ.tractography.utils import gen_seeds
 
@@ -51,7 +50,7 @@ def gpu_track(
         Float or binary mask describing the ROI within which we seed for
         tracking.
     pve_path : str
-        Estimations of partial volumes of WM, GM, and CSF.
+        Esimations of partial volumes of WM, GM, and CSF.
     odf_model : str, optional
         One of {"OPDT", "CSA"}
     seed_threshold : float
@@ -87,6 +86,7 @@ def gpu_track(
     -------
     """
     seed_img = nib.load(seed_path)
+    directions = directions.lower()
 
     # Roughly handle ACT/CMC for now
     wm_threshold = 0.01
@@ -103,14 +103,15 @@ def gpu_track(
 
     seed_data = seed_img.get_fdata()
 
-    theta = sphere.theta
-    phi = sphere.phi
-
     if directions == "boot":
-        sh_order_max = 6
-        full_basis = False
+        if odf_model.lower() == "opdt":
+            dg = BootDirectionGetter.from_dipy_opdt(gtab, sphere)
+        elif odf_model.lower() == "csa":
+            dg = BootDirectionGetter.from_dipy_csa(gtab, sphere)
+        else:
+            raise ValueError((f"odf_model must be 'opdt' or 'csa', not {odf_model}"))
     else:
-        # Determine sh_order and full_basis
+        # Convert SH coefficients to ODFs
         sym_order = (-3.0 + np.sqrt(1.0 + 8.0 * data.shape[3])) / 2.0
         if sym_order.is_integer():
             sh_order_max = sym_order
@@ -120,69 +121,27 @@ def gpu_track(
             sh_order_max = full_order
             full_basis = True
 
-    sampling_matrix, _, _ = shm.real_sh_descoteaux(
-        sh_order_max, theta, phi, full_basis=full_basis, legacy=False
-    )
+        theta = sphere.theta
+        phi = sphere.phi
 
-    if directions == "boot":
-        if odf_model.lower() == "opdt":
-            model_type = cuslines.ModelType.OPDT
-            model = OpdtModel(
-                gtab, sh_order_max=sh_order_max, smooth=0.006, min_signal=1
-            )
-            fit_matrix = model._fit_matrix
-            delta_b, delta_q = fit_matrix
-        elif odf_model.lower() == "csa":
-            model_type = cuslines.ModelType.CSA
-            model = CsaOdfModel(
-                gtab, sh_order_max=sh_order_max, smooth=0.006, min_signal=1
-            )
-            fit_matrix = model._fit_matrix
-            delta_b = fit_matrix
-            delta_q = fit_matrix
-        else:
-            raise ValueError((f"odf_model must be 'opdt' or 'csa', not {odf_model}"))
-    else:
-        if directions == "prob":
-            model_type = cuslines.ModelType.PROB
-        else:
-            model_type = cuslines.ModelType.PTT
+        sampling_matrix, _, _ = shm.real_sh_descoteaux(
+            sh_order_max, theta, phi, full_basis=full_basis, legacy=False
+        )
         model = shm.SphHarmModel(gtab)
         model.cache_set("sampling_matrix", sphere, sampling_matrix)
         model_fit = shm.SphHarmFit(model, data, None)
         data = model_fit.odf(sphere).clip(min=0)
-        delta_b = sampling_matrix
-        delta_q = sampling_matrix
 
-    b0s_mask = gtab.b0s_mask
-    dwi_mask = ~b0s_mask
-    x, y, z = model.gtab.gradients[dwi_mask].T
-    _, theta, phi = shm.cart2sphere(x, y, z)
-    B, _, _ = shm.real_sym_sh_basis(sh_order_max, theta, phi)
-    H = shm.hat(B)
-    R = shm.lcr_matrix(H)
-
-    gpu_tracker = cuslines.GPUTracker(
-        model_type,
-        radians(max_angle),
-        1.0,
-        float(wm_threshold),
-        float(step_size),
-        0.25,  # relative peak threshold
-        radians(45),  # min separation angle
-        np.ascontiguousarray(data).astype(np.float64),
-        H.astype(np.float64),
-        R.astype(np.float64),
-        delta_b.astype(np.float64),
-        delta_q.astype(np.float64),
-        b0s_mask.astype(np.int32),
-        np.ascontiguousarray(wm_data).astype(np.float64),
-        sampling_matrix.astype(np.float64),
-        sphere.vertices.astype(np.float64),
-        sphere.edges.astype(np.int32),
-        ngpus=ngpus,
-        rng_seed=0,
-    )
+        if directions == "ptt":
+            # Set FOD to 0 outside mask for probing
+            data[wm_data < wm_threshold, :] = 0
+            dg = PttDirectionGetter()
+        elif directions == "prob":
+            dg = ProbDirectionGetter()
+        else:
+            raise ValueError(
+                (f"directions must be 'boot', 'ptt', or 'prob', not {directions}")
+            )
 
     seeds = gen_seeds(
         seed_data,
@@ -194,77 +153,20 @@ def gpu_track(
         np.eye(4),
     )
 
-    global_chunk_sz = chunk_size * ngpus
-    nchunks = (seeds.shape[0] + global_chunk_sz - 1) // global_chunk_sz
-
-    # TODO: this code duplicated with GPUStreamlines...
-    # should probably be moved up to trx or cudipy at some point
-    if use_trx:
-        # Will resize by a factor of 2 if these are exceeded
-        sl_len_guess = 100
-        sl_per_seed_guess = 3
-        n_sls_guess = sl_per_seed_guess * seeds.shape[0]
-
-        # trx files use memory mapping
-        trx_file = TrxFile(
-            reference=seed_img,
-            nb_streamlines=n_sls_guess,
-            nb_vertices=n_sls_guess * sl_len_guess,
-        )
-        trx_file.streamlines._offsets = trx_file.streamlines._offsets.astype(np.uint64)
-        offsets_idx = 0
-        sls_data_idx = 0
-
-        with tqdm(total=seeds.shape[0]) as pbar:
-            for idx in range(int(nchunks)):
-                streamlines = gpu_tracker.generate_streamlines(
-                    seeds[idx * global_chunk_sz : (idx + 1) * global_chunk_sz]
-                )
-                tractogram = Tractogram(streamlines, affine_to_rasmm=seed_img.affine)
-                tractogram.to_world()
-                sls = tractogram.streamlines
-
-                new_offsets_idx = offsets_idx + len(sls._offsets)
-                new_sls_data_idx = sls_data_idx + len(sls._data)
-
-                if (
-                    new_offsets_idx > trx_file.header["NB_STREAMLINES"]
-                    or new_sls_data_idx > trx_file.header["NB_VERTICES"]
-                ):
-                    print("TRX resizing...")
-                    trx_file.resize(
-                        nb_streamlines=new_offsets_idx * 2,
-                        nb_vertices=new_sls_data_idx * 2,
-                    )
-
-                # TRX uses memmaps here
-                trx_file.streamlines._data[sls_data_idx:new_sls_data_idx] = sls._data
-                trx_file.streamlines._offsets[offsets_idx:new_offsets_idx] = (
-                    sls_data_idx + sls._offsets
-                )
-                trx_file.streamlines._lengths[offsets_idx:new_offsets_idx] = (
-                    sls._lengths
-                )
-
-                offsets_idx = new_offsets_idx
-                sls_data_idx = new_sls_data_idx
-                pbar.update(
-                    seeds[idx * global_chunk_sz : (idx + 1) * global_chunk_sz].shape[0]
-                )
-        trx_file.resize()
-
-        return trx_file
-    else:
-        streamlines_ls = [None] * nchunks
-        with tqdm(total=seeds.shape[0]) as pbar:
-            for idx in range(int(nchunks)):
-                streamlines_ls[idx] = gpu_tracker.generate_streamlines(
-                    seeds[idx * global_chunk_sz : (idx + 1) * global_chunk_sz]
-                )
-                pbar.update(
-                    seeds[idx * global_chunk_sz : (idx + 1) * global_chunk_sz].shape[0]
-                )
-
-        sft = StatefulTractogram(concatenate(streamlines_ls, 0), seed_img, Space.VOX)
-
-        return sft
+    with GPUTracker(
+        dg,
+        data,
+        wm_data,
+        wm_threshold,
+        sphere.vertices,
+        sphere.edges,
+        max_angle=radians(max_angle),
+        step_size=step_size,
+        ngpus=ngpus,
+        rng_seed=rng_seed,
+        chunk_size=chunk_size,
+    ) as gpu_tracker:
+        if use_trx:
+            return gpu_tracker.generate_trx(seeds, seed_img)
+        else:
+            return gpu_tracker.generate_sft(seeds, seed_img)
